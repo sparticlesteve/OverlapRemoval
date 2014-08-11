f6cd275ab424c37760f497d679f534e1318daa72
Steve Farrell <sfarrell@cern.ch>
2014-08-11 16:28:41 -0700

    Adding some OR functionality
    Defined some of the method forms which will be filled in later. Also
    added a couple of utility methods: a deltaR calculation and
    isSurvivingObject method which will use AuxElement::Accessor. Finally,
    added some of the necessary EDM dependencies.

--- OverlapRemoval/OverlapRemovalTool.h
+++ OverlapRemoval/OverlapRemovalTool.h
@@ -4,6 +4,12 @@
 // Framework includes
 #include "AsgTools/AsgTool.h"
 
+// EDM includes
+#include "xAODBase/IParticle.h"
+#include "xAODEgamma/ElectronContainer.h"
+#include "xAODJet/JetContainer.h"
+#include "xAODMuon/MuonContainer.h"
+
 // Local includes
 #include "OverlapRemoval/IOverlapRemovalTool.h"
 
@@ -40,11 +46,43 @@ class OverlapRemovalTool : public virtual IOverlapRemovalTool,
     /// @name Methods implementing the IOverlapRemovalTool interface
     /// @{
 
+    /// Remove overlapping electrons and jets
+    /// This method will decorate both the electrons and jets according to
+    /// both the e-jet and jet-e overlap removal prescriptions.
+    virtual void removeEleJetOverlap(const xAOD::ElectronContainer* electrons,
+                                     const xAOD::JetContainer* jets);
+
+    /// Remove overlapping muons and jets
+    virtual void removeMuonJetOverlap(const xAOD::MuonContainer* muons,
+                                      const xAOD::JetContainer* jets);
+
+    /// Remove overlapping electrons and muons
+    /// TODO: make it possible to veto event based on this. Maybe the
+    /// return value should just be a bool.
+    virtual void removeEleMuonOverlap(const xAOD::ElectronContainer* electrons,
+                                      const xAOD::MuonContainer* muons);
+
     /// @}
 
   protected:
 
+    /// Recommended calculation of overlap distance parameter, delta R.
+    /// delta R = sqrt((y1-y2)^2 + (phi1-phi2)^2).
+    /// This is in contrast to what is returned by TLorentzVector::DeltaR,
+    /// which uses the pseudorapidity eta rather than the rapidity y.
+    double deltaR(const xAOD::IParticle* p1, const xAOD::IParticle* p2);
+
+    /// TODO: add methods for selecting objects by decoration.
+    /// For example, isSurvivingJet(jet) which will return true if
+    /// the pass decoration is set or if the output decoration is missing.
+    bool isSurvivingObject(const xAOD::IParticle* obj);
+
   private:
+
+    /// TODO: add configurable properties for
+    /// - input and output decorations
+    /// - OR parameters (dR, etc.)
+    /// - OR scheme choices
 
 }; // class OverlapRemovalTool
 
--- Root/OverlapRemovalTool.cxx
+++ Root/OverlapRemovalTool.cxx
@@ -1,3 +1,6 @@
+// EDM includes
+#include "AthContainers/AuxElement.h"
+
 // Local includes
 #include "OverlapRemoval/OverlapRemovalTool.h"
 
@@ -16,4 +19,53 @@ OverlapRemovalTool::OverlapRemovalTool(const std::string& name)
 StatusCode OverlapRemovalTool::initialize()
 {
   return StatusCode::SUCCESS;
+}
+
+//-----------------------------------------------------------------------------
+// Remove overlapping electrons and jets
+//-----------------------------------------------------------------------------
+void OverlapRemovalTool::removeEleJetOverlap
+(const xAOD::ElectronContainer* electrons, const xAOD::JetContainer* jets)
+{
+}
+
+//-----------------------------------------------------------------------------
+// Remove overlapping muons and jets
+//-----------------------------------------------------------------------------
+void OverlapRemovalTool::removeMuonJetOverlap
+(const xAOD::MuonContainer* muons, const xAOD::JetContainer* jets)
+{
+}
+
+//-----------------------------------------------------------------------------
+// Remove overlapping electrons and muons
+//-----------------------------------------------------------------------------
+void OverlapRemovalTool::removeEleMuonOverlap
+(const xAOD::ElectronContainer* electrons, const xAOD::MuonContainer* muons)
+{
+}
+
+//-----------------------------------------------------------------------------
+// Calculate delta R between two particles
+//-----------------------------------------------------------------------------
+double OverlapRemovalTool::deltaR(const xAOD::IParticle* p1,
+                                  const xAOD::IParticle* p2)
+{
+  double dY = p1->rapidity() - p2->rapidity();
+  double dPhi = TVector2::Phi_mpi_pi(p1->phi() - p2->phi());
+  return sqrt(dY*dY + dPhi*dPhi);
+}
+
+//-----------------------------------------------------------------------------
+// Determine if object is currently surviving OR.
+// Surviving means the object either hasn't been seen yet
+// or the 'pass' decoration is set to 1
+//-----------------------------------------------------------------------------
+bool OverlapRemovalTool::isSurvivingObject(const xAOD::IParticle* obj)
+{
+  static SG::AuxElement::ConstAccessor<int> passAcc("passOR");
+  if(!passAcc.isAvailable(*obj) || passAcc(*obj) == 1){
+    return true;
+  }
+  return false;
 }
